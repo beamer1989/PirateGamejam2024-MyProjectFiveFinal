e1017579477afdee09895f8702c2bf5b919339c6
Vamtaro <ambrosiano.laura@gmail.com>
2024-01-13 13:57:35 -0800

    Update movement.cs

--- Assets/Scripts/movement.cs
+++ Assets/Scripts/movement.cs
@@ -24,6 +24,7 @@ public class movement : MonoBehaviour
         //    Debug.Log("boop");
         //        }
        
+        // update deez nuuutss //
 
         Vector2 newPosition = this.transform.position;
 
