fcbdfadb9101fe1b1f2888b0490fd2633c5fdc01
Vamtaro <ambrosiano.laura@gmail.com>
2024-01-13 13:48:50 -0800

    Initial Commit
    we did a move around

---
+++ Assets/Scripts/movement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public float speed;
+    public int updog;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+    
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (Input.GetKey(KeyCode.W)) 
+        //{
+        //    Debug.Log("boop");
+        //        }
+       
+
+        Vector2 newPosition = this.transform.position;
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            newPosition.y += speed;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            newPosition.y -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            newPosition.x -= speed;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            newPosition.x += speed;
+        }
+
+        this.transform.position = newPosition;
+    }
+}
