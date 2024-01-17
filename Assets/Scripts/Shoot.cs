f7ccfa2f4aabfca44b2a1827c66e8bbf9db9bc1d
FoxHound1216 <seanwallacesd@gmail.com>
2024-01-16 23:48:12 -0800

    Added shooting physics
    -added force to slime
    -added physics to slime
    -added ammo
    -added ammo regen
    -added cooldown to slime
    -added splat effect

--- Assets/Scripts/Shoot.cs
+++ Assets/Scripts/Shoot.cs
@@ -11,13 +11,29 @@ public class Shoot : MonoBehaviour
     private GameObject slimebeingfired;
     public Camera cam;
 
+    public float cooldowntime = 2;
+    public float ammotimer;
+    public int maxammo = 5;
+    public int currentammo;
+    public float shoottimer;
+    public float shootcooldown = 0.1f;
+
     Vector2 mousePos;
+
+    public float slimeForce = 25f;
+
+    //Update called at start
+    private void Start()
+    {
+        currentammo = maxammo;
+    }
+
 
     // Update is called once per frame
     void Update()
     {
         //Left Click
-        if (Input.GetMouseButtonDown(0))
+        if (Input.GetMouseButtonDown(0) && currentammo > 0 && shoottimer > shootcooldown)
         {
             Debug.Log("Mouse 0 - Left Click");
 
@@ -35,18 +51,45 @@ public class Shoot : MonoBehaviour
             var dir = Input.mousePosition - Camera.main.WorldToScreenPoint(transform.position);
             var angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
             slimebeingfired.transform.rotation = Quaternion.AngleAxis(angle, Vector3.forward);
-           
-            //TO DO ** Add Force
 
             //Determines where the mouse position is
             mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+
+            //TO DO ** Add Force
+            Rigidbody2D rb = slimebeingfired.GetComponent<Rigidbody2D>();
+            Vector2 playerpos = new Vector2(this.transform.position.x, this.transform.position.y);
+            rb.AddForce((mousePos - playerpos).normalized * slimeForce, ForceMode2D.Impulse);
+
+            //Explodes Slime
+            Destroy(slimebeingfired, 0.5f);
+
+            //Removes 1 from ammo counter
+            currentammo --;
+
+            //Resets shoot timer
+            shoottimer = 0;
+        }
+        regenerateammo();
+
+        shoottimer += Time.deltaTime;
+
+    }
+
+    void regenerateammo()
+    {
+        if(currentammo >= maxammo)
+        {
+            return;
+        }
+    if(ammotimer > cooldowntime)
+        {
+            currentammo ++;
+            ammotimer = 0;
+        }
+        else
+        {
+            ammotimer = ammotimer + Time.deltaTime;
         }
     }
-    //void FixedUpdate()
-    //{
-    //    Vector2 lookDir = mousePos - rb.position;
-    //    float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
 
-    //}
-        
 }
