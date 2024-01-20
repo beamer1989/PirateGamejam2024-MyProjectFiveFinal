0213a5307a405273ecf878af216e72bfee5448a2
Vamtaro <ambrosiano.laura@gmail.com>
2024-01-19 21:49:06 -0800

    Programmed Start Menu

---
+++ Assets/Scripts/Menu.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Menu : MonoBehaviour
+{
+    public void OnPlayButton()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void OnControlsButton()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void OnQuitButton()
+    {
+        Application.Quit();
+    }
+
+    public void OnBackToMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
